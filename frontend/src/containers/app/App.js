9fe92eff8791167b08748ad20173a554d61619b1
nik1168 <ngeisser32@gmail.com>
2020-03-22 00:09:46 +0100

    Adding enzyme to perform unit testing from now on and doing first test in app component.

--- frontend/src/containers/app/App.js
+++ frontend/src/containers/app/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {bindActionCreators} from "redux";
 import {connect} from 'react-redux'
 import './App.css';
 import PlayersContainer from "../../components/players_container/PlayersContainer";
 import * as playersActions from '../../actions/players'
 
+export class App extends Component {
+    render() {
+        const {players} = this.props;
+        return (
+            <div className="list-players">
+                <div className="list-players-title">
+                    <h1>Tennis Game</h1>
+                </div>
+                <div className="list-players-content">
+                    <div>
+                        <PlayersContainer title={"Players"} isMatch={false} players={players}/>
+                    </div>
 
-function App(props) {
-    const {players} = props;
-    return (
-        <div className="list-players">
-            <div className="list-players-title">
-                <h1>Tennis Game</h1>
-            </div>
-            <div className="list-players-content">
-                <div>
-                    <PlayersContainer title={"Players"} isMatch={false} players={players}/>
+                    <div>
+                        {
+                            <PlayersContainer title={"Match"} isMatch={true} players={[]}/>
+                        }
+
+                    </div>
                 </div>
 
-                <div>
-                    <PlayersContainer title={"Match"} isMatch={true} players={[]}/>
-                </div>
             </div>
-
-        </div>
-    );
+        )
+    }
 }
 
 function mapStateToProps(state) {
-    console.log(state.players.players);
     return {
         players: state.players.players
     }
--- frontend/src/containers/app/App.test.js
+++ frontend/src/containers/app/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import {shallow} from 'enzyme'
+import {App} from "./App"; // Notice that we import the unconnected component
+
+it('Displays the correct title', () => {
+    // We use wrapper to denote a react component that is being wrapped
+    const wrapper = shallow(<App/>);
+    const title = wrapper.find('h1').text();
+    expect(title).toEqual("Tennis Game")
 });
