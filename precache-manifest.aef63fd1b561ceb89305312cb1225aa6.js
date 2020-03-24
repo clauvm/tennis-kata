25494ce6d56dace8286e2ef62e0ad622aad6aba4
nik1168 <ngeisser32@gmail.com>
2020-03-25 00:35:38 +0100

    Updates

---
+++ precache-manifest.aef63fd1b561ceb89305312cb1225aa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eff455db8bcd63250f50727766ca7e0",
+    "url": "/tennis-kata/index.html"
+  },
+  {
+    "revision": "c93928cc3ff11b6c041c",
+    "url": "/tennis-kata/static/css/main.a7cdb12b.chunk.css"
+  },
+  {
+    "revision": "6f8ea91223ddf11a1b43",
+    "url": "/tennis-kata/static/js/2.e63bab22.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/tennis-kata/static/js/2.e63bab22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93928cc3ff11b6c041c",
+    "url": "/tennis-kata/static/js/main.c17fe051.chunk.js"
+  },
+  {
+    "revision": "98861051aaeb7e12576b",
+    "url": "/tennis-kata/static/js/runtime-main.54098988.js"
+  },
+  {
+    "revision": "53d31e5256988273c6267b0bc7419b48",
+    "url": "/tennis-kata/static/media/play.53d31e52.svg"
+  }
+]);
